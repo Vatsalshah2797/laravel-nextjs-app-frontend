ad5cac3af5991a4933cf9e161ae6f41345bf4b60
vatsalp.shah <vatsalp.shah@brainvire.com>
2023-03-13 03:27:39 +0530

    next js laravel todo list api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    BACKEND_API_URL:'http://localhost:8001/api'
+  }
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -38,6 +39,8 @@ export default function Home() {
             </a>
           </div>
         </div>
+
+        <Link href='/todo'>Todo App</Link>
 
         <div className={styles.center}>
           <Image
